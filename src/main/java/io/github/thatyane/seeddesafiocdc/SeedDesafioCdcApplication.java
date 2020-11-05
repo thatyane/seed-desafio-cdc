fd774ea74ba88eba47ef20b53ea5dfbf8b9e162d
Thatyane Pontes <thatyanepontes@gmail.com>
2020-11-05 19:00:11 -0300

    create project

---
+++ src/main/java/io/github/thatyane/seeddesafiocdc/SeedDesafioCdcApplication.java
@@ -0,0 +1,13 @@
+package io.github.thatyane.seeddesafiocdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedDesafioCdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeedDesafioCdcApplication.class, args);
+    }
+
+}
